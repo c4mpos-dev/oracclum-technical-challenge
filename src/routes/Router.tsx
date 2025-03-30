6850a7a4d7afb88d109176d35ba225f5d502b5a8
Cauã Campos <campos.eet@gmail.com>
2025-03-30 00:34:59 -0300

    :wrench: Deploy failed

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
+import { RedirectToHome } from "../components/RedirectToHome.tsx";
 import { ProtectedRoutes } from "./ProtectedRoutes.tsx";
 
 import { SignIn } from "../pages/SignIn.tsx";
@@ -12,7 +13,7 @@ export function Router() {
             <Route path="/login" element={<SignIn />} />
             <Route path="/register" element={<SignUp />} />
             <Route element={<ProtectedRoutes />}>
-                <Route path="/" />
+                <Route path="/" element={<RedirectToHome />}/>
                 <Route path="/questions" element={<Questions />} />
             </Route>
         </Routes>
